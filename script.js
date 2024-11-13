88b8583de59c989bd4996ed80a7bb05f3d100b04
alessiotinaglia <alessio.tina1998@gmail.com>
2024-11-13 16:59:58 +0100

    fine es bonus

--- script.js
+++ script.js
@@ -42,41 +42,58 @@ const teamMembers = [
 
 const profilo = document.querySelector(".profilo")
 
-teamMembers.forEach(element => {
+
+const addCardToDOM = (membro) => {
     profilo.innerHTML += `
     <div class="col-12 col-md-6 col-lg-6 mb-4 col-xl-4">
-    <div class="card d-flex flex-row bg-black">
-        <img src="${element.img}" class="card-img" alt="Immagine della card">
+        <div class="card d-flex flex-row bg-black">
+            <img src="${membro.img}" class="card-img" alt="Immagine della card">
             <div class="card-body d-flex flex-column justify-content-between bg-black">
-                <h5 class="card-title text-white">${element.name}</h5>
-                <p class="card-text text-white">${element.role}</p>
-                <a href="#">${element.email}</a>
+                <h5 class="card-title text-white">${membro.name}</h5>
+                <p class="card-text text-white">${membro.role}</p>
+                <a href="#">${membro.email}</a>
             </div>
+        </div>
     </div>
-</div>
-`
-});
+    `;
+};
+
+teamMembers.forEach(addCardToDOM);
 
 // bonus
 
 const form = document.getElementById("form");
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
     event.preventDefault(); 
 
     // Ottieni i valori dal form
     const name = document.getElementById("name").value;
     const role = document.getElementById("role").value;
     const email = document.getElementById("email").value;
-    const img = document.getElementById("img").value;
+    const img = "img/Stephen-Hawking.jpg"; 
 
-    teamMembers.push({
+    // Stampiamo i valori nella console
+    console.log("Nome: " + name);
+    console.log("Cognome: " + role);
+    console.log("email: " + email);
+    console.log("foto: " + img);
+
+    // Crea un nuovo oggetto 
+    const nuovoMembro = {
         name: name,
         role: role,
         email: email,
         img: img
-    });
+    };
+    // Aggiungi il nuovo membro all'array teamMembers
+    teamMembers.push(nuovoMembro);
 
-    // Rende visibile il nuovo membro
-    renderTeamMembers();
+    console.log("Nuovo membro aggiunto:", nuovoMembro);
+
+    // Aggiungi la nuova card al DOM
+    addCardToDOM(nuovoMembro);
+
+    // Reset del form
+    form.reset();
 });
